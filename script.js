3321b7c8c0d72d46a9cfb37760f85c6bdba2101d
LL8719 <arsgatslr_87@yahoo.com>
2023-02-14 20:12:49 -0700

    added Team project and hover text to each work item

---
+++ script.js
@@ -0,0 +1,5 @@
+$('.surf-container').attr('title', 'Team project');
+$('.calc').attr('title', 'Weather Dashboard');
+$('.pastel').attr('title', 'Work Day Scheduler');
+$('.led-wall').attr('title', 'Horison Website');
+$('.run-buddy').attr('title', 'Coding Quiz');
