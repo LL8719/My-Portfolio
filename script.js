98388adc69afb97adb64572ecf5eebd3a5702b0c
LL8719 <arsgatslr_87@yahoo.com>
2023-04-13 18:49:43 -0700

    updated portfolio with team project

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $('.surf-container').attr('title', 'Team project');
 $('.calc').attr('title', 'Weather Dashboard');
 $('.pastel').attr('title', 'Work Day Scheduler');
-$('.led-wall').attr('title', 'Horison Website');
+$('.led-wall').attr('title', 'Passport Adventures');
 $('.run-buddy').attr('title', 'Coding Quiz');
